95fedbfb63d2a839fc5c5c86b1830f72f66431f0
SheebaHashmi <sheebahashmi0@gmail.com>
2022-01-10 17:15:14 -0500

    Create POST endpoint

--- api/server.js
+++ api/server.js
@@ -7,6 +7,24 @@ const server = express();
 server.use(express.json());
 
 //ENDPOINTS
+//POST
+server.post('/api/users',async(req,res) => {
+    try{
+        const { name,bio } = req.body
+        console.log(name,bio)
+        if(!name || !bio){
+            res.status(400).json({ message: "Please provide name and bio for the user" })
+        }else{
+            const newUser = await Users.insert({ name,bio })
+            console.log(newUser)
+            res.status(201).json(newUser)
+        }
+
+    }
+    catch{
+        res.status(500).json({ message: "There was an error while saving the user to the database" })
+    }
+})
 
 //GET
 server.get('/api/users', async (req,res)=> {

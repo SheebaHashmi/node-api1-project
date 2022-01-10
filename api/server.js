728b6b50575fe4dfe35cb2fef6bf69f448822003
SheebaHashmi <sheebahashmi0@gmail.com>
2022-01-10 17:49:46 -0500

    Create GET by id endpoint

--- api/server.js
+++ api/server.js
@@ -28,9 +28,30 @@ server.post('/api/users',async(req,res) => {
 
 //GET
 server.get('/api/users', async (req,res)=> {
+    // const id = req.params.id;
+    // console.log(id)
     try{
         const users = await Users.find()
-        res.json(users)
+        res.json(users)  
+    }
+    catch{
+        res.status(500).json({ message: "The user information could not be retrieved" })
+    }
+})
+//GET request with id
+
+server.get('/api/users/:id',async (req,res) => {
+    try{
+        const { id } =  req.params
+        // console.log(id)
+        const user = await Users.findById(id)
+        if(!user){
+            res.status(404).json({ message: "The user with the specified ID does not exist" })
+        }
+        else{
+            // console.log(user);
+             res.json(user)
+        }
     }
     catch{
         res.status(500).json({ message: "The user information could not be retrieved" })

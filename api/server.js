6f7d4a6592cc4b23e8bc99d1d5fbcb047d544f30
SheebaHashmi <sheebahashmi0@gmail.com>
2022-01-10 17:03:08 -0500

    Create get endpoint

--- api/server.js
+++ api/server.js
@@ -1,3 +1,22 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const Users = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+
+server.use(express.json());
+
+//ENDPOINTS
+
+//GET
+server.get('/api/users', async (req,res)=> {
+    try{
+        const users = await Users.find()
+        res.json(users)
+    }
+    catch{
+        res.status(500).json({ message: "The user information could not be retrieved" })
+    }
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
